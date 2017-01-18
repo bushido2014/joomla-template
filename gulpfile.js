1a08cb927a8fc34bb5b46a2119ffddb5ec7b59c0
Viorel Soltan <Viorel Soltan>
2017-01-18 21:46:44 +0200

    add browsersync imagemin custom files scss

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var $    = require('gulp-load-plugins')();
 var concat = require('gulp-concat');
 var cssnano = require('gulp-cssnano');
 var rename = require('gulp-rename');
+var browserSync = require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 
 var sassPaths = [
   'bower_components/normalize.scss/sass',
@@ -11,7 +14,7 @@ var sassPaths = [
 ];
 
 gulp.task('sass', function() {
-  return gulp.src('scss/app.scss')
+  return gulp.src('scss/*.scss')
     .pipe($.sass({
       includePaths: sassPaths,
       outputStyle: 'compressed' // if css compressed **file size**
@@ -21,10 +24,33 @@ gulp.task('sass', function() {
       browsers: ['last 2 versions', 'ie >= 9']
     }))
     .pipe(cssnano())
-    .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
+    .pipe(rename({suffix: '.min'})) 
     .pipe(gulp.dest('css'));
 });
 
-gulp.task('default', ['sass'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
+
+gulp.task('imagemin', function () {
+    return gulp.src('images/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('images'));
 });
+
+gulp.task('serve',['sass'] ,function () {
+
+    
+browserSync.init({
+        proxy: "joomla-test.dev",
+        notify: false
+    });
+
+    gulp.watch("*.php").on("change", browserSync.reload);
+    gulp.watch("scss/*.scss", ['sass']).on("change", browserSync.reload);
+    gulp.watch("js/**/*.js").on("change", browserSync.reload);
+});
+gulp.task('default',['sass', 'serve']);
+
+
